41bde94bef874a0153e4c584b390693597175b89
Goh Matsumoto <urimaro@gmail.com>
2018-01-28 17:49:34 +0900

    P15

--- 2.2.exs
+++ 2.2.exs
@@ -6,3 +6,12 @@ IO.inspect b
 #=> 2
 IO.inspect c
 #=> 3
+
+IO.inspect list = [ 1, 2, [ 3, 4, 5 ] ]
+IO.inspect [ a, b, c ] = list
+IO.inspect a
+#=> 1
+IO.inspect b
+#=> 2
+IO.inspect c
+#=> [ 3, 4, 5 ]
