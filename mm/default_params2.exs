78931f47746d6f82c3be69fd0f07f69aa39be4c4
Goh Matsumoto <urimaro@gmail.com>
2018-02-25 23:23:12 +0900

    P53 デフォルトパラメータを含むヘッドだけをボディなしで追加する 後の関数定義では通常のパラメータを使う

---
+++ mm/default_params2.exs
@@ -0,0 +1,12 @@
+defmodule Params do
+  def func(p1, p2 \\ 123)
+
+  def func(p1, p2) when is_list(p1) do
+    "you said #{p2} with a list"
+  end
+
+  def func(p1, p2) do
+    "you passed in #{p1} and #{p2}"
+  end
+end
+
