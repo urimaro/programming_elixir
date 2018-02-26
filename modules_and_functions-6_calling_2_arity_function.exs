a62e4964022c281c20a1e56328da98cb6a535353
Goh Matsumoto <urimaro@gmail.com>
2018-02-26 18:51:34 +0900

    P53 ModulesAndFunctions-6 privateな関数を使った場合と、公開されている関数を使った場合を実装した privateな関数を使うと関数を呼び出す回数は減るが、わかりにくい感じがする どちらが良いのだろう？

---
+++ modules_and_functions-6_calling_2_arity_function.exs
@@ -0,0 +1,24 @@
+defmodule Chop do
+  def guess(n, a..b) do
+    guess(n, a..b, middle(a, b))
+  end
+
+  defp guess(expect, _, expect) do
+    IO.puts "It is #{expect}"
+  end
+
+  defp guess(n, a.._, expect) when n < expect do
+    IO.puts "It is #{expect}"
+    guess(n, a..(expect - 1))
+  end
+
+  defp guess(n, _..b, expect) when n > expect do
+    IO.puts "It is #{expect}"
+    guess(n, (expect + 1)..b)
+  end
+
+  defp middle(a, b) do
+    a + div(b - a, 2)
+  end
+end
+
