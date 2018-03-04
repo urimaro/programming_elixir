ef862afe0c5c85e679b574324301dca67f8037d4
Goh Matsumoto <urimaro@gmail.com>
2018-03-04 14:21:44 +0900

    P66 7.3 ヘッドとテイルを使ったリストの構築

--- lists/mylist1.exs
+++ lists/mylist1.exs
@@ -1,5 +1,8 @@
 defmodule MyList do
-  def len([]), do: 0
+  def len([]),             do: 0
   def len([_head | tail]), do: 1 + len(tail)
+
+  def square([]),            do: []
+  def square([head | tail]), do: [head * head | square(tail)]
 end
 
